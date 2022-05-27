79d9ffef18b4743cc5acf65da9a1c4e76912587f
Ryan <104562337+railBitsAndHex@users.noreply.github.com>
2022-05-27 13:53:47 +0800

    Set up config for multi chain

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -25,19 +25,64 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
+console.log(process.env.PRIVATE_KEY1);
+const privateKey1: string = process.env.PRIVATE_KEY1!;
+const privateKey2: string = process.env.PRIVATE_KEY2!;
+const privateKey3: string = process.env.PRIVATE_KEY3!;
+
+const accounts: Array<string> = [privateKey1, privateKey2, privateKey3];
+
+// RPC URLS
+const GOERLI_URL: string = process.env.ALCHEMY_GOERLI_RPC_URL!;
+const MUMBAI_URL: string = process.env.ALCHEMY_MUMBAI_RPC_URL!;
+const ARBRINKEBY_URL: string = process.env.ALCHEMY_ARBRINKEBY_RPC_URL!;
+const MOONBASE_URL: string = "https://rpc.api.moonbase.moonbeam.network";
+const FUJI_URL: string = "https://api.avax-test.network/ext/bc/C/rpc";
 const config: HardhatUserConfig = {
-  solidity: "0.8.4",
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  defaultNetwork: "hardhat",
   networks: {
     hardhat: {
       chainId: 31337,
     },
     localhost: {
       chainId: 31337,
+      accounts: accounts.length !== 0 ? accounts : [],
     },
-    ropsten: {
-      url: process.env.ROPSTEN_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    goerli: {
+      chainId: 5,
+      accounts: accounts.length !== 0 ? accounts : [],
+      url: GOERLI_URL !== undefined ? GOERLI_URL : "",
+    },
+    mumbai: {
+      chainId: 80001,
+      accounts: accounts.length !== 0 ? accounts : [],
+      url: MUMBAI_URL !== undefined ? MUMBAI_URL : "",
+    },
+    arbRinkeby: {
+      chainId: 421611,
+      accounts: accounts.length !== 0 ? accounts : [],
+      url: ARBRINKEBY_URL !== undefined ? ARBRINKEBY_URL : "",
+      gas: 7000000,
+      gasPrice: 200000000,
+    },
+    moonbase: {
+      chainId: 1287,
+      accounts: accounts.length !== 0 ? accounts : [],
+      url: MOONBASE_URL,
+    },
+    fuji: {
+      chainId: 43113,
+      accounts: accounts.length !== 0 ? accounts : [],
+      url: FUJI_URL,
     },
   },
   gasReporter: {
@@ -52,6 +97,12 @@ const config: HardhatUserConfig = {
       default: 0,
       1: 0,
     },
+    secondary: {
+      default: 2,
+    },
+  },
+  mocha: {
+    timeout: 200000, // 200 seconds max for running tests
   },
 };
 
