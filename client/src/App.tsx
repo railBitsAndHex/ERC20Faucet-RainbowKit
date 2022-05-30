2980198f23ebb77c5511c52000d6949ae529a28d
Ryan <104562337+railBitsAndHex@users.noreply.github.com>
2022-05-31 01:51:32 +0800

    Added connect btn

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,18 @@
 import React from "react";
+import "@rainbow-me/rainbowkit/styles.css";
+
 import "./App.css";
 import { WagmiConfig } from "wagmi";
-import { RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { RainbowKitProvider, darkTheme } from "@rainbow-me/rainbowkit";
 import { wagmiClient, chains } from "./configs/config";
+import ConnectBtn from "./components/ConnectBtn";
 function App() {
   return (
     <WagmiConfig client={wagmiClient}>
-      <RainbowKitProvider chains={chains}>
+      <RainbowKitProvider chains={chains} coolMode>
         <section>
-          <h1>Hello world!</h1>
+          <h1>ERC20 MockToken Faucet</h1>
+          <ConnectBtn />
         </section>
       </RainbowKitProvider>
     </WagmiConfig>
