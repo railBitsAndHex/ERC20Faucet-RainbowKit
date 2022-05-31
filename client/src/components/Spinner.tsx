b7c1a44132c07168b9ccdac818714d939a5fcc6b
Ryan <104562337+railBitsAndHex@users.noreply.github.com>
2022-05-31 13:56:33 +0800

    Added spinner

---
+++ client/src/components/Spinner.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "../styles/spinner.modules.css";
+function Spinner() {
+  return (
+    <div className="lds-roller">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
+
+export default Spinner;
